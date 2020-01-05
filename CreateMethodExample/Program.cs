b9d48bbee260a21d9b606c112b61d6f0ec4f0a2d
山﨑 謙次郎 <kenjiro.yamasaki@gmail.com>
2020-01-06 08:50:45 +0900

    [update]プラットフォームを.NET Framework 4.7.2 に変更

--- CreateMethodExample/Program.cs
+++ CreateMethodExample/Program.cs
@@ -19,15 +19,15 @@ namespace Mono.Cecil.CreateMethodExample
                     var module = assembly.Modules.Single(m => m.Name == "SoftCube.Assembly.exe");
                     var type   = module.Types.Single(t => t.FullName == "SoftCube.Class");
 
-                    var method = new MethodDefinition("Test", MethodAttributes.Private | MethodAttributes.Static, assembly.MainModule.TypeSystem.Void);
+                    var method = new MethodDefinition("Test", MethodAttributes.Private | MethodAttributes.Static, module.TypeSystem.Void);
                     type.Methods.Add(method);
 
                     var processor    = method.Body.GetILProcessor();
                     var instructions = method.Body.Instructions;
 
                     instructions.Insert(0, processor.Create(OpCodes.Ldstr, "Hello!"));
-                    instructions.Insert(1, Instruction.Create(OpCodes.Call, module.ImportReference(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }))));
-                    instructions.Add(Instruction.Create(OpCodes.Ret));
+                    instructions.Insert(1, processor.Create(OpCodes.Call, module.ImportReference(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }))));
+                    instructions.Add(processor.Create(OpCodes.Ret));
 
                     assembly.Write(outputAssemblyFileName, new WriterParameters() { WriteSymbols = true });
                 }
